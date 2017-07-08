30df402691867c44061e378e9c3f117d21f69282
Хамелеон <maxali946@gmail.com>
2017-07-08 09:58:02 +0400

    100%

--- Tasks/Tasks/Functions.h
+++ Tasks/Tasks/Functions.h
@@ -1,5 +1,7 @@
 #pragma once
 
-void menu(char **tasks, int size);
-//void load(FILE *f, char **tasks, char * path, int &size);
-//void save(FILE *f, char **tasks, char * path, int size);
+//extern FILE *f;
+//----------------------------------------------------
+void menu(char **&tasks, int &size);
+void load(FILE * f, char **&tasks, char * path, int &size);
+void save(FILE * f, char **tasks, char * path, int size);
